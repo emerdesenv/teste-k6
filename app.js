a8f3ba8d0f99a518009f2a242829b457d64441b7
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-01-16 17:11:52 -0300

    refactor: Mais um teste

--- app.js
+++ app.js
@@ -6,41 +6,26 @@ const routes = require('./src/routes');
 
 // Criando a aplicação Express
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 const uri = process.env.MONGODB_URI;
 
-if (!uri) {
-    console.error('Erro: MONGODB_URI não está definido no arquivo .env');
-    process.exit(1);
-}
+// Conectando ao MongoDB
+mongoose.connect(uri)
+.then(() => {
+    console.log('Conectado ao MongoDB');
+}).catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+});
 
 // Middleware para permitir JSON nas requisições
 app.use(express.json());
 
-// Definindo um endpoint de prontidão
-app.get('/health', (req, res) => res.status(200).send('OK'));
+// Usando as rotas definidas em routes.js
+app.use('/api', routes);
 
-// Função para iniciar o servidor
-async function startServer() {
-    try {
-        console.log('Conectando ao MongoDB...');
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Conectado ao MongoDB.');
+// Iniciando o servidor
+const PORT = process.env.PORT || 3000;
 
-        // Usando as rotas definidas em routes.js
-        app.use('/api', routes);
-
-        app.listen(PORT, () => {
-            console.log(`Servidor rodando na porta ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Erro ao conectar ao MongoDB:', error.message);
-        process.exit(1); // Encerra o processo em caso de falha
-    }
-}
-
-// Inicializar o servidor
-startServer();
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
