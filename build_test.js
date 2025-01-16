852ab82ae3b754f354c939ceeae737f5ed961edc
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-01-16 16:47:01 -0300

    refactor: Somente um teste4

---
+++ build_test.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./src/routes');
+
+// Criando a aplicação Express
+const app = express();
+
+const uri = __ENV.MONGODB_URI;
+
+// Conectando ao MongoDB
+mongoose.connect(uri)
+.then(() => {
+    console.log('Conectado ao MongoDB');
+}).catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+});
+
+// Middleware para permitir JSON nas requisições
+app.use(express.json());
+
+// Usando as rotas definidas em routes.js
+app.use('/api', routes);
+
+// Iniciando o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
